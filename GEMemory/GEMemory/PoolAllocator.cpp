4d2e6d8c843a1d7df09898aa1d35e03715e9eede
Arvid Wahlgren <salliban03@gmail.com>
2025-11-18 08:21:57 +0100

    Created Node struct and corrected some issues in init function

--- GEMemory/GEMemory/PoolAllocator.cpp
+++ GEMemory/GEMemory/PoolAllocator.cpp
@@ -19,13 +19,16 @@ bool PoolAllocator::Init(int n, int size)
 	_size = size;
 
 	_address = malloc(static_cast<size_t>(n * size));
-	_nodes = (int*)malloc(static_cast<size_t>(n * sizeof(int)));
+	_nodes = (Node*)malloc(static_cast<size_t>(n) * sizeof(Node));
 
+	_head = 0;
 	if (_nodes != nullptr)
 	{
 		for (int i = 0; i < n; i++)
 		{
-			_nodes[i] = -1;
+			_nodes[i].free = true;
+			if (i == n - 1) _nodes[i].next = -1;
+			else _nodes[i].next = i + 1;
 		}
 	}
 
--- GEMemory/GEMemory/PoolAllocator.h
+++ GEMemory/GEMemory/PoolAllocator.h
@@ -1,5 +1,10 @@
 #pragma once
 
+struct Node {
+	bool free;	// true if part of the list, otherwise false
+	int next;	// index of next node in the linked list
+};
+
 class PoolAllocator
 {
 private:
@@ -7,7 +12,7 @@ private:
 	int _head = 0; // Index of the first free slot
 	int _n = -1; // Number of blocks
 	int _size = -1; // Size of a block
-	int *_nodes = nullptr; // Indices of the next used slot
+	Node *_nodes = nullptr; // Indices of the next used slot
 
 	bool Expand();
 
