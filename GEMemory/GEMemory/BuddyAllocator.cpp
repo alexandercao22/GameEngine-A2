8fd524d69f6d1f0502b0a2e53947f4dd6c78df79
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-11-23 15:49:36 +0100

    Implemented BuddyAllocator::Init()

--- GEMemory/GEMemory/BuddyAllocator.cpp
+++ GEMemory/GEMemory/BuddyAllocator.cpp
@@ -1,5 +1,9 @@
 #include "BuddyAllocator.h"
 
+#include <iostream>
+#include <cmath>
+#include <malloc.h>
+
 BuddyAllocator::~BuddyAllocator()
 {
 
@@ -7,7 +11,46 @@ BuddyAllocator::~BuddyAllocator()
 
 bool BuddyAllocator::Init(unsigned int size)
 {
+	// Check if size is of base 2
+	float log2Size = std::log(size) / std::log(2);
+	if (ceilf(log2Size) != log2Size) {
+		std::cout << "BuddyAllocator::Init(): Failed to initalize" << std::endl;
+		return false;
+	}
 
+	_memory = malloc(size);
+	if (!_memory) {
+		std::cerr << "BuddyAllocator::Init(): Failed to allocate memory" << std::endl;
+		return false;
+	}
+
+	// Calculate how many slots are needed
+	for (int i = 0; i < log2Size - (std::log(_maxDepthSize) / std::log(2)) + 1; i++) {
+		_numBuddies += std::pow(2, i);
+	}
+	_buddies = (Buddy *)malloc(static_cast<size_t>(sizeof(Buddy) * _numBuddies));
+	if (!_buddies) {
+		std::cerr << "BuddyAllocator::Init(): Failed to allocate buddies" << std::endl;
+		return false;
+	}
+
+	int level = 0;
+	int buddiesToInit = std::pow(2, level); // Amount of buddies to initialize on current level
+	int buddiesInit = 0; // Amount of buddies initialized on current level
+	for (int i = 0; i < _numBuddies; i++) {
+		_buddies[i].size = size / std::pow(2, level);
+		_buddies[i].state = 0;
+		_buddies[i].ptr = (char *)_memory + (_buddies[i].size * buddiesInit);
+
+		buddiesInit++;
+		if (buddiesInit == buddiesToInit) {
+			level++;
+			buddiesToInit = std::pow(2, level);
+			buddiesInit = 0;
+		}
+	}
+
+	return true;
 }
 
 void *BuddyAllocator::Request(unsigned int size)
