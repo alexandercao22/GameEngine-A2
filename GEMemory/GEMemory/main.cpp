19c89b13a37eecb45a5229a6dd9e71336d417132
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-11-23 15:57:28 +0100

    Implemented a test scenario for BuddyAllocator

--- GEMemory/GEMemory/main.cpp
+++ GEMemory/GEMemory/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "PoolAllocator.h"
+#include "BuddyAllocator.h"
 
 struct Enemy
 {
@@ -11,58 +12,72 @@ struct Enemy
 int main() {
 	std::cout << "Hello World!" << std::endl;
 
-	PoolAllocator firstPool;
-	firstPool.Init(5, 9);
-	PoolAllocator secondPool;
-	secondPool.Init(5, 32, true);
+	//PoolAllocator firstPool;
+	//firstPool.Init(5, 9);
+	//PoolAllocator secondPool;
+	//secondPool.Init(5, 32, true);
 
-	std::cout << "Första pointers adress: " << firstPool.GetAdress() << std::endl;
+	//std::cout << "Första pointers adress: " << firstPool.GetAdress() << std::endl;
 
-	Enemy* firstPtr = (Enemy *)firstPool.Request();
-	firstPtr->health = 69.67f;
-	firstPtr->legs = 2;
-	firstPtr->tag = 'b';
+	//Enemy* firstPtr = (Enemy *)firstPool.Request();
+	//firstPtr->health = 69.67f;
+	//firstPtr->legs = 2;
+	//firstPtr->tag = 'b';
 
-	std::cout << "First value: " << firstPtr->health << std::endl;
-	std::cout << "First value: " << firstPtr->legs << std::endl;
-	std::cout << "First value: " << firstPtr->tag << std::endl;
-	std::cout << "Första pointers adress: " << firstPtr << std::endl;
+	//std::cout << "First value: " << firstPtr->health << std::endl;
+	//std::cout << "First value: " << firstPtr->legs << std::endl;
+	//std::cout << "First value: " << firstPtr->tag << std::endl;
+	//std::cout << "Första pointers adress: " << firstPtr << std::endl;
 
-	Enemy *secondPtr = (Enemy *)firstPool.Request();
-	firstPtr->health = 12.34f;
-	firstPtr->legs = 27;
-	firstPtr->tag = 'y';
+	//Enemy *secondPtr = (Enemy *)firstPool.Request();
+	//firstPtr->health = 12.34f;
+	//firstPtr->legs = 27;
+	//firstPtr->tag = 'y';
 
-	std::cout << "Second value: " << firstPtr->health << std::endl;
-	std::cout << "Second value: " << firstPtr->legs << std::endl;
-	std::cout << "Second value: " << firstPtr->tag << std::endl;
-	std::cout << "Andra pointers adress: " << secondPtr << std::endl;
+	//std::cout << "Second value: " << firstPtr->health << std::endl;
+	//std::cout << "Second value: " << firstPtr->legs << std::endl;
+	//std::cout << "Second value: " << firstPtr->tag << std::endl;
+	//std::cout << "Andra pointers adress: " << secondPtr << std::endl;
 
-	void* thirdPtr = firstPool.Request();
+	//void* thirdPtr = firstPool.Request();
 
-	std::cout << "Tredje pointers adress: " << thirdPtr << std::endl;
+	//std::cout << "Tredje pointers adress: " << thirdPtr << std::endl;
 
-	void* fourthPtr = firstPool.Request();
+	//void* fourthPtr = firstPool.Request();
 
-	std::cout << "Fjärde pointers adress: " << fourthPtr << std::endl;
+	//std::cout << "Fjärde pointers adress: " << fourthPtr << std::endl;
 
-	void* fifthPtr = firstPool.Request();
+	//void* fifthPtr = firstPool.Request();
 
-	std::cout << "Femte pointers adress: " << fifthPtr << std::endl;
+	//std::cout << "Femte pointers adress: " << fifthPtr << std::endl;
 
-	firstPool.Free(secondPtr);
+	//firstPool.Free(secondPtr);
 
-	void* sixthPtr = firstPool.Request();
+	//void* sixthPtr = firstPool.Request();
 
-	std::cout << "Sjätte pointers adress: " << sixthPtr << std::endl;
-	
+	//std::cout << "Sjätte pointers adress: " << sixthPtr << std::endl;
+	//
 
-	void* seventhPtr = firstPool.Request();
-	
-	std::cout << "Sjunde pointers adress: " << seventhPtr << std::endl;
+	//void* seventhPtr = firstPool.Request();
+	//
+	//std::cout << "Sjunde pointers adress: " << seventhPtr << std::endl;
 
-	std::cout << "SecondPool: " << secondPool.GetAdress() << std::endl;
+	//std::cout << "SecondPool: " << secondPool.GetAdress() << std::endl;
 
+	BuddyAllocator buddyAllocator;
+	buddyAllocator.Init(512);
+	void *ptr1 = buddyAllocator.Request(32);
+	buddyAllocator.PrintStates();
+	void *ptr2 = buddyAllocator.Request(64);
+	buddyAllocator.PrintStates();
+	void *ptr3 = buddyAllocator.Request(30);
+	buddyAllocator.PrintStates();
+
+	std::cout << "Start ptr: " << buddyAllocator.GetAddress() << std::endl;
+	std::cout << "Ptr1:      " << ptr1 << std::endl;
+	std::cout << "Ptr2:      " << ptr2 << std::endl;
+	std::cout << "Ptr3:      " << ptr3 << std::endl;
+	std::cout << "Diff:      " << (char *)ptr2 - ptr1 << std::endl;
 
 	return 0;
 }
