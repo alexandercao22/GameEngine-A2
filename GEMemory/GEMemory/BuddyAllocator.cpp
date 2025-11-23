c2d00a88f98c69ca95a52e2fa33a6415c6222f67
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-11-23 15:46:46 +0100

    Created BuddyAllocator class

---
+++ GEMemory/GEMemory/BuddyAllocator.cpp
@@ -0,0 +1,27 @@
+#include "BuddyAllocator.h"
+
+BuddyAllocator::~BuddyAllocator()
+{
+
+}
+
+bool BuddyAllocator::Init(unsigned int size)
+{
+
+}
+
+void *BuddyAllocator::Request(unsigned int size)
+{
+
+}
+
+bool BuddyAllocator::Free(void *element)
+{
+	return false;
+}
+
+void *BuddyAllocator::GetAddress()
+{
+
+}
+
