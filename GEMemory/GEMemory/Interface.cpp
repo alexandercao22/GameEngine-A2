f4df3d7af1ad9d10fd84875a35eeb997e5396372
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-11-28 16:12:48 +0100

    Implemented Interface initialize functions

--- GEMemory/GEMemory/Interface.cpp
+++ GEMemory/GEMemory/Interface.cpp
@@ -21,14 +21,32 @@ void Interface::ShowStackInfo()
 
 Interface::Interface()
 {
+	Init();
 }
 
 Interface::~Interface()
 {
+	for (PoolContainer pCon : _pools) {
+		delete pCon.pool;
+	}
+	for (BuddyContainer bCon : _buddies) {
+		delete bCon.buddy;
+	}
+	for (StackContainer sCon : _stacks) {
+		delete sCon.stack;
+	}
 }
 
 bool Interface::Init()
 {
+	std::srand(std::time({}));
+
+	InitWindow(_width, _height, "Game Engine Assignment 2");
+	SetTargetFPS(60);
+
+	rlImGuiSetup(true);
+
+	return false;
 }
 
 void Interface::Update()
