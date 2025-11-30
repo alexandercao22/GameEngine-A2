cfb9e5032affa68328c561420fc3a747da0f1f9d
HalvEmil <carlemilhalvarsson@gmail.com>
2025-11-30 15:24:15 +0100

    fixed my *our* proj problems

--- GEMemory/GEMemory/TestCases.h
+++ GEMemory/GEMemory/TestCases.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "Objects.h"
 
-struct Enemy {
-	float health = 100.0f;
-	int legs = 3;
-	char tag = 'a';
-};
+//struct Enemy {
+//	float health = 100.0f;
+//	int legs = 3;
+//	char tag = 'a';
+//};
 
 
 void PoolVSOS();
