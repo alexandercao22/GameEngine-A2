e15cb8ddcce74c46eb245fd0364d7f46c3114a74
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-12-02 12:54:05 +0100

    Removed included unused libraries

--- GEMemory/GEMemory/main.cpp
+++ GEMemory/GEMemory/main.cpp
@@ -1,17 +1,7 @@
-#include <iostream>
-#include "PoolAllocator.h"
-#include "StackAllocator.h"
-#include "BuddyAllocator.h"
 #include "TestCases.h"
-
 #include "Interface.h"
-#include "Objects.h"
-
-#include <cstdlib>
 
 #include "raylib.h"
-#include "imgui.h"
-#include "rlImGui.h"
 
 int main() {
 	Interface interface;
